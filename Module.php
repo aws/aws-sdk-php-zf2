5e2a721c914a9caa0968e6fa96d1d9416e57ac4c
Julien Guittard <julien.guittard@me.com>
2016-07-05 00:07:35 -0400

    Bumped Module version to current release

--- Module.php
+++ Module.php
@@ -9,7 +9,7 @@ use Zend\ModuleManager\Feature\ConfigProviderInterface;
  */
 class Module implements ConfigProviderInterface
 {
-    const VERSION = '2.0.1';
+    const VERSION = '2.1.0';
 
     /**
      * {@inheritdoc}
